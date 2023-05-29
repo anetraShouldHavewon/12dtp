aac77a4f88e872e25adb306abc549ba9b4dfafde
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-29 16:46:42 +1200

    did sum testing and finally started writing routes

--- database_testing.py
+++ database_testing.py
@@ -2,8 +2,24 @@ import sqlite3
 conn = sqlite3.connect("Movie_Database_1.db")
 cursor = conn.cursor()
 
-for id in range(1,5):
-    genre_id = cursor.execute("SELECT genre_id FROM Movie_Genre WHERE movie_id = ?", (id,)).fetchall()
-    print(genre_id)
-    
-    li_genre_id = []
+#GETTING MOVIE ID FROM ITS NAME#
+#movie_name = str.title(input("movie name: "))
+#while movie_name not in [" ",""]:
+    #movie_id = cursor.execute("SELECT id FROM Movie WHERE title = ?",(movie_name,)).fetchone()
+    #print(movie_id)
+    #movie_name = str.title(input("movie name: "))
+#else:
+    #print("See you")
+
+movie_id = input("movie id?: ")
+if movie_id.isdigit():
+    genre_ids = cursor.execute("SELECT genre_id FROM Movie_Genre WHERE movie_id = ?", (movie_id,)).fetchall()
+    print(genre_ids)
+else:
+    print("movie id not valid")
+
+genre_list = []
+for genre_id in genre_ids:
+    genre_list.append(genre_id[0])
+print(genre_list)
+
