98546a1006489865971ccdfb4923f6c8806f6f92
tramdang628 <tramdang628@gmail.com>
2023-06-04 23:43:29 +1200

    worked on routes.py

--- routes.py
+++ routes.py
@@ -1,1 +1,27 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+
+@app.route("/movie_info/<int:id>")
+def movie_info(id):
+
+@app.route("/explore")
+def explore():
+
+@app.route("/quiz_question/<int:question_num")
+def quiz_question(question_num):
+
+@app.route("/quiz_results")
+def quiz_results():
+
+@app.route("/my_watchlist")
+def my_watchlist():
+
+@app.route("/to_watch_list")
+def to_watch_list():
+
+@app.route("/watched_list")
+def watched_list():
