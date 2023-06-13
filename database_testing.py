a2068222e5ffa9d6788e4418ab5e56aa57ea79da
anetraShouldHavewon <tramdang628@gmail.com>
2023-06-13 21:01:40 +1200

    wrote some tests for homepage+ changed the movie table in the database

--- database_testing.py
+++ database_testing.py
@@ -2,24 +2,15 @@ import sqlite3
 conn = sqlite3.connect("Movie_Database_1.db")
 cursor = conn.cursor()
 
-#GETTING MOVIE ID FROM ITS NAME#
-#movie_name = str.title(input("movie name: "))
-#while movie_name not in [" ",""]:
-    #movie_id = cursor.execute("SELECT id FROM Movie WHERE title = ?",(movie_name,)).fetchone()
-    #print(movie_id)
-    #movie_name = str.title(input("movie name: "))
-#else:
-    #print("See you")
-
-movie_id = input("movie id?: ")
-if movie_id.isdigit():
-    genre_ids = cursor.execute("SELECT genre_id FROM Movie_Genre WHERE movie_id = ?", (movie_id,)).fetchall()
-    print(genre_ids)
+genre_name = input("genre_name: ").title()
+genre_id = cursor.execute("SELECT id FROM Genre WHERE name = ?",(genre_name,)).fetchone()
+if genre_id == None:
+    print("genre_name is invalid")
 else:
-    print("movie id not valid")
+    genre_id = genre_id[0]
+    print(genre_id)
 
-genre_list = []
-for genre_id in genre_ids:
-    genre_list.append(genre_id[0])
-print(genre_list)
+genre_name = input("genre_name: ").title()
+genre_id = cursor.execute("SELECT id FROM Genre WHERE name = ?",(genre_name,)).fetchone()
+genre_id = genre_id[0]
 
--- routes.py
+++ routes.py
@@ -1,31 +1,76 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
+#homepage
 @app.route("/home")
 def home():
+    conn = sqlite3.connect("Movie_Database_1.db")
+    cur = conn.cursor()
 
-@app.route("/movie_info/<int:id>")
-def movie_info(id):
+    #getting a list of agenre ids from a list of names of genres
+    genre_list = ["Horro", "Romance", "Martial Arts"]
+    genre_id_list = []
+    for genre_name in genre_list:
+        genre_name = genre_name.title()
+        genre_id = cur.execute("SELECT id FROM Genre WHERE name = ?",(genre_name,)).fetchone()
+        if genre_id == None:
+            print("genre_name is invalid")  #test print line
+        else:
+            genre_id = genre_id[0]
+            genre_id_list.append(genre_id)
+    print(genre_id_list)    #another test print line
 
-@app.route("/explore")
-def explore():
+    #getting a list of movie ids and their namesfrom a list of genre ids
+    #getting movie_ids from a genre_id
+    def movie_id_from_genre_id(genre_id):
+        movie_id_list = []
+        movie_ids = cur.execute("SELECT movie_id FROM Movie_Genre WHERE genre_id = ?",(genre_id,)).fetchall()
+        for movie_id in range(len(movie_ids)):
+            movie_id_list.append(movie_ids[movie_id][0])
+        return movie_id_list
 
-@app.route("/quiz_question/<int:question_num")
-def quiz_question(question_num):
+    #getting movie names from movie_ids
+    def movie_name_from_genre_id(movie_id_list):
+        movie_names_list = []
+        for movie_id in movie_id_list:
+            movie_name = cur.execute("SELECT title FROM Movie WHERE id = ?",(movie_id,)).fetchone()
+            movie_names_list.append(movie_name[0])
+        return movie_names_list
+    
+    movie_id_dict = {}
+    movie_names_dict = {}
 
-@app.route("/quiz_results")
-def quiz_results():
+    for id in range(len(genre_id_list)):
+        genre_id = genre_id_list[id]
+        movie_id_list = movie_id_from_genre_id(genre_id)
+        movie_id_dict[genre_list[id]] = movie_id_list
+        movie_name_list = movie_name_from_genre_id(movie_id_list)
+        movie_names_dict[genre_list[id]] = movie_name_list
 
-@app.route("/my_watchlist")
-def my_watchlist():
+    return render_template("home.html", genre_ids = genre_id_list, movie_ids = movie_id_dict, movie_names = movie_names_dict)
 
-@app.route("/to_watch_list")
-def to_watch_list():
+#@app.route("/movie_info/<int:id>")
+#def movie_info(id):
 
-@app.route("/watched_list")
-def watched_list():
+#@app.route("/explore")
+#def explore():
+
+#@app.route("/quiz_question/<int:question_num")
+#def quiz_question(question_num):
+
+#@app.route("/quiz_results")
+#def quiz_results():
+
+#@app.route("/my_watchlist")
+#def my_watchlist():
+
+#@app.route("/to_watch_list")
+#def to_watch_list():
+
+#@app.route("/watched_list")
+#def watched_list():
 
 if __name__ == "__main__":
     app.run(debug = True)
